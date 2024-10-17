4aef7c513bf4cea1be3ef884f9c135ae6f624588
Samuel Okoro Chka <okorosamuelchika@gmail.com>
2024-10-17 09:28:25 +0100

    fix Rendered more hooks than during the previous render error

--- src/app/(tabs)/groups/[id].tsx
+++ src/app/(tabs)/groups/[id].tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Stack, useLocalSearchParams } from 'expo-router';
 import { Main } from 'tamagui.config';
 import {
@@ -6,17 +6,16 @@ import {
   Text,
   Button,
   Avatar,
+  Spinner,
+  Tabs,
   Separator,
   SizableText,
-  Tabs,
-  TabsContentProps,
   H5,
-  Spinner,
+  TabsContentProps,
 } from 'tamagui';
 import { useFetchGroupById, useJoinGroup } from '@/api/groups';
 import ScreenSpinner from '@/components/ScreenSpinner';
-import { useEffect, useState } from 'react';
-import { Alert, Pressable } from 'react-native';
+import { Alert } from 'react-native';
 import { useAuth, useFetchUserById } from '@/api/auth';
 
 const TabsContent = (props: TabsContentProps) => {
@@ -39,46 +38,84 @@ const TabsContent = (props: TabsContentProps) => {
   );
 };
 
-export default function groupScreen() {
+const GroupTabs = () => (
+  <Tabs
+    defaultValue="tab1"
+    orientation="horizontal"
+    flexDirection="column"
+    width={'100%'}
+    height={600}
+    backgroundColor={'whitesmoke'}
+    overflow="hidden"
+    borderColor="$borderColor">
+    <Tabs.List
+      separator={<Separator vertical />}
+      disablePassBorderRadius="bottom"
+      aria-label="Group Tabs">
+      <Tabs.Tab flex={1} value="tab1">
+        <SizableText fontFamily="$body">Exercise</SizableText>
+      </Tabs.Tab>
+      <Tabs.Tab flex={1} value="tab2">
+        <SizableText fontFamily="$body">Members</SizableText>
+      </Tabs.Tab>
+    </Tabs.List>
+    <Separator />
+    <TabsContent value="tab1">
+      <H5 color={'black'}>Exercise</H5>
+    </TabsContent>
+
+    <TabsContent value="tab2">
+      <H5 color={'black'}>Members</H5>
+    </TabsContent>
+  </Tabs>
+);
+
+export default function GroupScreen() {
   const { id: idStr } = useLocalSearchParams();
   const groupId = typeof idStr === 'string' ? idStr : idStr[0];
   const { data: groupData, isLoading, error } = useFetchGroupById(groupId);
   const { data: adminData } = useFetchUserById(groupData?.created_by);
   const { data: sessionData } = useAuth();
-  const [isMember, setIsMember] = useState(true);
+  const [isMember, setIsMember] = useState(false);
   const { mutate: joinGroup, isPending, error: joinGroupError } = useJoinGroup();
-
-  if (!groupId) return;
-  if (isLoading) return <ScreenSpinner />;
-  if (error) return <Text m="auto">{error.message}</Text>;
 
   const userId = sessionData?.user?.id;
 
   useEffect(() => {
-    const checkMembership = () => {
-      if (!groupData || !userId) return;
-
+    if (groupData && userId) {
       const isUserMember =
         groupData.memberIds.includes(userId) || groupData.adminIds.includes(userId);
       setIsMember(isUserMember);
-    };
-
-    checkMembership();
+    }
   }, [groupData, userId]);
+
+  const handleJoinGroup = () => {
+    if (userId) {
+      joinGroup(
+        { group_id: groupId, user_id: userId },
+        {
+          onSuccess: () => {
+            setIsMember(true);
+          },
+        }
+      );
+    }
+  };
+
+  if (!groupId) return null;
+  if (isLoading) return <ScreenSpinner />;
+  if (error) return <Text m="auto">{error.message}</Text>;
 
   const handleLeaveGroup = () => {
     Alert.alert(
       'Leave Group',
       'Are you sure you want to leave this group?',
       [
-        {
-          text: 'Cancel',
-          style: 'cancel',
-        },
+        { text: 'Cancel', style: 'cancel' },
         {
           text: 'OK',
           onPress: () => {
-            // Perform leave group action
+            /* Perform leave group action */
           },
         },
       ],
@@ -86,31 +123,6 @@ export default function groupScreen() {
     );
   };
 
-  const handleJoinGroup = () => {
-    joinGroup(
-      { group_id: groupId, user_id: userId ?? '' },
-      {
-        onSuccess: () => {
-          setIsMember(false);
-        },
-      }
-    );
-  };
-
-  if (joinGroupError) {
-    console.log(joinGroupError.message);
-
-    return (
-      <View m="auto" gap="$2">
-        <Text color="red" fontSize={15}>
-          Something went wrong, please try again..
-        </Text>
-        <Button width={'50%'} margin={'auto'} textAlign="center" onPress={handleJoinGroup}>
-          {isPending ? <Spinner color={'white'} /> : 'Join Group'}
-        </Button>
-      </View>
-    );
-  }
   return (
     <Main marginVertical={isMember ? 0 : 'auto'}>
       <Stack.Screen options={{ headerShown: false }} />
@@ -119,11 +131,9 @@ export default function groupScreen() {
           <Text fontSize={25} fontWeight={'700'}>
             {groupData?.name}
           </Text>
-
           <Text fontSize={15} fontWeight={'500'} color={'gray'} alignItems="center">
             {groupData?.description}
           </Text>
-
           <View flexDirection="row" alignItems="center" justifyContent="space-between">
             <View justifyContent="center" gap={5} alignItems="baseline" marginBottom={20}>
               <Text fontWeight={'500'}>Created By</Text>
@@ -162,36 +172,17 @@ export default function groupScreen() {
           </View>
         </View>
 
-        {isMember ? (
-          <Tabs
-            defaultValue="tab1"
-            orientation="horizontal"
-            flexDirection="column"
-            width={'100%'}
-            height={600}
-            backgroundColor={'whitesmoke'}
-            overflow="hidden"
-            borderColor="$borderColor">
-            <Tabs.List
-              separator={<Separator vertical />}
-              disablePassBorderRadius="bottom"
-              aria-label="Group Tabs">
-              <Tabs.Tab flex={1} value="tab1">
-                <SizableText fontFamily="$body">Exercise</SizableText>
-              </Tabs.Tab>
-              <Tabs.Tab flex={1} value="tab2">
-                <SizableText fontFamily="$body">Members</SizableText>
-              </Tabs.Tab>
-            </Tabs.List>
-            <Separator />
-            <TabsContent value="tab1">
-              <H5 color={'black'}>Exercise</H5>
-            </TabsContent>
-
-            <TabsContent value="tab2">
-              <H5 color={'black'}>Members</H5>
-            </TabsContent>
-          </Tabs>
+        {joinGroupError ? (
+          <View m="auto" gap="$2">
+            <Text color="red" fontSize={15}>
+              Something went wrong, please try again..
+            </Text>
+            <Button width={'50%'} margin={'auto'} textAlign="center" onPress={handleJoinGroup}>
+              {isPending ? <Spinner color={'white'} /> : 'Join Group'}
+            </Button>
+          </View>
+        ) : isMember ? (
+          <GroupTabs />
         ) : (
           <Button width={'50%'} margin={'auto'} onPress={handleJoinGroup}>
             {isPending ? <Spinner color={'white'} /> : 'Join Group'}
